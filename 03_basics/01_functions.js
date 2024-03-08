6cab65c2980902c1154f81c5686369d15f7c5ab0
the_coding_bear <akanksha060801@gmail.com>
2024-03-09 00:38:16 +0530

    function with object and array are added

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,8 +18,8 @@ function addTwoNumbers(no1,no2){
    // return result
    return no1+no2
 }
- const result=addTwoNumbers(5,45)
- console.log("Result: ",result);
+ // const result=addTwoNumbers(5,45)
+// console.log("Result: ",result);
 
  function loginUserMessage(username){
     if(username==undefined){
@@ -30,4 +30,34 @@ function addTwoNumbers(no1,no2){
  }
 
  // console.log(loginUserMessage("Akanksha"))
- console.log(loginUserMessage("Akanksha"));
+ // console.log(loginUserMessage("Akanksha"));
+
+ function calculateCartPrice(val1,val2,...num1){
+    return num1
+ }
+  // console.log(calculateCartPrice(200,500,40))
+
+  const user={
+    username:"Akanksha",
+    price: 199
+  }
+
+  function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price }`);
+ }
+ // handleObject( user)
+ handleObject(
+    {
+        username:"Akku",
+        price: 399
+    }
+ )
+
+ const myNewArray = [200,400,100,600]
+
+ function returnSecondValue(getArray){
+    return getArray[1]
+ }
+
+//  console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
