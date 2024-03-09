8cfea5576b7922f593330ff40553b5c6af7e2e56
the_coding_bear <akanksha060801@gmail.com>
2024-03-09 11:49:20 +0530

    scope level and mini hoisting are added

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,15 +1,49 @@
 // var c=300
 let a=300
 if(true){
-let a=10
-const b=20
- console.log("INNER=",a);
+    let a=10
+    const b=20
+ //console.log("INNER=",a);
 }
 
-for(let i=0;i<array.length;i++){
-    const element = array[i];
-}
 
-console.log(a);
+
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username="codingbear"
+
+    function two(){
+        const website ="github"
+       // console.log(username);
+    }
+   // console.log(website);
+
+    two()
+}
+//one()
+
+if(true){
+    const username="codingbear"
+    if(username==="codingbear"){
+        const website ="github"
+        //console.log(username+website);
+    }
+    //console.log(website);
+}
+// console.log(username);
+ 
+//+++++++++++++++++++++interesting++++++++++++++++++
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+
+
+const addTwo=function (num) {
+    return num + 2
+}
+addTwo(5)
