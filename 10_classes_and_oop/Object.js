c78117f59fd4d0b5fd58ce2880dd2a6a8e7ecb61
the_coding_bear <akanksha060801@gmail.com>
2024-03-27 10:05:10 +0530

    new keyword are added

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -17,8 +17,10 @@ createUser.prototype.increment = function(){
     this.score++
 }
 createUser.prototype.printMe = function(){
-    console.log(`score is ${this.score}`);
+    console.log(`price is ${this.score}`);
 }
 
-const acon = createUser("akku",24)
+const acon =  new createUser("akku",24)
 const bcon = createUser("abbu",250)
+ 
+acon.printMe()
