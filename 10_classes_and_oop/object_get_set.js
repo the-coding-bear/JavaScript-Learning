2517bdb62f4ae3adbe109f9c3d9678eee5b857c4
the_coding_bear <akanksha060801@gmail.com>
2024-03-30 11:39:06 +0530

    object- get and set are added

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,16 @@
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const ob1 = Object.create(User)
+console.log(ob1.email);
